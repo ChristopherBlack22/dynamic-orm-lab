e2be9d422a721780af91a8de45e9f5f65fb295dc
Christopher <christopher.black.2209@googlemail.com>
2021-07-16 02:20:36 +0100

    commit tree 345e796fd8c6c4d72c1262fc2755d8cd45a67850

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,65 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end 
+
+    def self.column_names
+        #DB[:conn].results_as_hash = true
+        sql = "PRAGMA table_info('#{self.table_name}')"
+        table_info = DB[:conn].execute(sql)
+        column_names = []
+        table_info.each do |column|
+            column_names << column["name"]
+        end
+        column_names.compact
+    end 
+
+    def initialize(attributes_hash = {})
+        attributes_hash.each {|key, value| self.send("#{key}=", value)}
+    end 
+
+    def table_name_for_insert
+        self.class.table_name 
+    end 
+
+    def col_names_for_insert
+        self.class.column_names.select {|name| name != "id"}.join(", ")
+    end 
+
+    def values_for_insert
+        values = []
+        keys = self.class.column_names.select {|name| name != "id"}
+        keys.each { |key| values << "'#{send(key)}'" }
+        values.join(", ")
+    end
+
+    def question_marks_for_insert
+        array = []
+        number = self.class.column_names.count-1
+        number.times { array << "?"}
+        array.join(", ")
+    end 
+
+    def save
+        sql = <<-SQL
+            INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) 
+            VALUES (#{values_for_insert})
+        SQL
+        DB[:conn].execute(sql)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+    end 
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{table_name} WHERE name = ?"
+        DB[:conn].execute(sql, name)
+    end 
+
+    def self.find_by(attribute_hash)
+        sql = "SELECT * FROM #{table_name} WHERE #{attribute_hash.keys.join} = ? "
+        DB[:conn].execute(sql, attribute_hash.values)
+    end 
+
+
 end
